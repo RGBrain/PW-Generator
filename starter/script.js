29314ad16bebfb9ae714ab29379d1b9a516d5bc4
RGBrain <31857067+RGBrain@users.noreply.github.com>
2022-12-02 19:53:57 +0000

    WIP

--- starter/script.js
+++ starter/script.js
@@ -88,25 +88,58 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// TODO Create an array for user choices 
+// Create über-array 
+const totalArray = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters);
+
+// TODO Create an object for user choices 
 let userOptions = {
-  characters:,
-  
+  characters: 10,
+  special: true,
+  numeric: true,
+  lowercase: true,
+  uppercase: true
 };
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-  alert("How many characters would you like in your password?")
+  userOptions.characters = prompt("How many characters would you like in your password?")
+  userOptions.special = confirm("Include special characters?")
+  userOptions.numeric = confirm("Include numeric characters?")
+  userOptions.lowercase = confirm("Include lowercase characters?")
+  userOptions.uppercase = confirm("Include uppercase characters?")
 }
 
+let randomChoice;
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+ return totalArray[randomChoice = Math.floor(Math.random() * totalArray.length)];
 }
 
+// Create custom array in line with user choices 
+userOptions = {
+  characters: 10,
+  special: true,
+  numeric: true,
+  lowercase: true,
+  uppercase: true
+};
+
+// ? For Debugging
+getRandom();
+getPasswordOptions();
+console.log(userOptions)
+console.log(randomChoice)
+console.log(getRandom);
+
+let password;
 // Function to generate password with user input
 function generatePassword() {
-
+  for (i=0; i < userOptions.characters.length(); i++) {
+    password += getRandom
+  }
 }
+generatePassword();
+console.log(password);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
