107e96c6829abccc16d5af2a364cf5e0f155f15c
RGBrain <31857067+RGBrain@users.noreply.github.com>
2022-12-04 12:54:28 +0000

    MVP

--- starter/script.js
+++ starter/script.js
@@ -88,66 +88,67 @@ var uppercaseCharacters = [
   'Z'
 ];
 
-// Create an object for user choices 
-let userOptions = {
-  characters: 5,  // initial placeholder value
-  special: true,
-  numeric: true,
-  lowercase: true,
-  uppercase: true
-};
-
-do {
-  userOptions.characters = prompt("How many characters would you like in your password?")
-} while (userOptions.characters < 10 || userOptions.characters > 64)
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-  userOptions.special = confirm("Include special characters?");
-  userOptions.numeric = confirm("Include numeric characters?");
-  userOptions.lowercase = confirm("Include lowercase characters?");
-  userOptions.uppercase = confirm("Include uppercase characters?");
-}
-
-// Call password options function and validate that at least one character set has been chosen
-do {
-  getPasswordOptions();
-} while (userOptions.special === false && userOptions.numeric === false &&  userOptions.lowercase === false &&  userOptions.uppercase === false)
-
-// Create custom array 
-let totalArray = [];
-if (userOptions.special) {
-  totalArray = totalArray.concat(specialCharacters);
-}
-if (userOptions.numeric) {
-  totalArray = totalArray.concat(numericCharacters);
-}
-if (userOptions.lowercase) {
-  totalArray = totalArray.concat(lowercaseCharacters);
-}
-if (userOptions.uppercase) {
-  totalArray = totalArray.concat(uppercaseCharacters);
-}
-
-
-console.log("total array is now: " + totalArray);
-
-let randomChoice;
-// Function for getting a random element from an array
-function getRandom(arr) {
- return totalArray[randomChoice = Math.floor(Math.random() * totalArray.length)];
-}
-
-
+// Declare variables outside function 
 let password = "";
+let totalArray = [];
+
 // Function to generate password with user input
 function generatePassword() {
-  for (i = 0; i < userOptions.characters; i++) {
-    password += getRandom(totalArray);
-  }
-}
+    // Create an object for user choices 
+    let userOptions = {
+      characters: 5,  // initial placeholder value
+      special: true,
+      numeric: true,
+      lowercase: true,
+      uppercase: true
+    };
 
-generatePassword();
+    // ? Check password value
+    console.log(password);
+
+    do {
+      userOptions.characters = prompt("How many characters would you like in your password?")
+    } while (userOptions.characters < 10 || userOptions.characters > 64)
+
+    // Function to prompt user for password options
+    function getPasswordOptions() {
+      userOptions.special = confirm("Include special characters?");
+      userOptions.numeric = confirm("Include numeric characters?");
+      userOptions.lowercase = confirm("Include lowercase characters?");
+      userOptions.uppercase = confirm("Include uppercase characters?");
+    }
+
+    // Call password options function and validate that at least one character set has been chosen
+    do {
+      getPasswordOptions();
+    } while (userOptions.special === false && userOptions.numeric === false &&  userOptions.lowercase === false &&  userOptions.uppercase === false)
+
+    // Create custom array 
+    totalArray = [];
+    if (userOptions.special) {
+      totalArray = totalArray.concat(specialCharacters);
+    }
+    if (userOptions.numeric) {
+      totalArray = totalArray.concat(numericCharacters);
+    }
+    if (userOptions.lowercase) {
+      totalArray = totalArray.concat(lowercaseCharacters);
+    }
+    if (userOptions.uppercase) {
+      totalArray = totalArray.concat(uppercaseCharacters);
+    }
+    console.log(totalArray)  // ! THIS IS WORKING !
+    let randomChoice;
+    // Function for getting a random element from an array
+    function getRandom(arr) {
+    return totalArray[randomChoice = Math.floor(Math.random() * totalArray.length)];
+    }
+
+      for (i = 0; i < userOptions.characters; i++) {
+        password += getRandom(totalArray);
+      }
+      return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -159,6 +160,9 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// ? Check password value
+console.log(password);
 
 var passwordText = document.querySelector('#password');
 passwordText.value = password;
